5207a36003bf8743a2b8d9531ccac72101628c5d
Jonahlyn Gilstrap <jgilstrap@gmail.com>
2019-03-31 12:41:59 -0600

    Added area for an image and some basic styles.

--- app/app.py
+++ app/app.py
@@ -10,6 +10,7 @@ import numpy as np
 app = dash.Dash(__name__)
 server = app.server # the Flask app
 
+
 # Todo: Get some real data to work with
 import fakedata as fd
 df = fd.get_data('2019-01-01T00:00:00Z', '2019-02-01T00:00:00Z')
@@ -25,16 +26,15 @@ def generate_table(dframe, max_rows=10):
 
 def get_graph_layout(title = ""):
     return {
-        'title': '',
+        'title': title,
         'height': 100,
-        'width': 600,
-        'margin': {'l': 40, 'b': 25, 'r': 40, 't': 10},
+        'margin': {'l': 20, 'b': 25, 'r': 20, 't': 30},
         'xaxis': {
                   'tickvals': [0, 300, 600, 900, 1200, 1500, 1800, 2100],
                   'ticktext': ['', '3am', '6', '9', 'noon', '3pm', '6', '9'],
                    },
         'yaxis': {
-                  'title': title,
+                #   'title': title,
                   'visible': True,
                   'autorange': True,
                   'showgrid': False, 
@@ -42,17 +42,17 @@ def get_graph_layout(title = ""):
                   'showline': False, 
                   'ticks': '',
                   'showticklabels': False,
-                  'hoverformat': '.1f'}
+                  'hoverformat': '.1f'},
+        # 'layout': {
+        #     'annotations': [{
+        #        'x': 0, 'y': 0, 'xanchor': 'left', 'yanchor': 'bottom',
+        #        'xref': 'paper', 'yref': 'paper', 'showarrow': False,
+        #        'align': 'left', 'bgcolor': 'rgba(255, 255, 255, 0.5)',
+        #        'text': title }]
+        # }        
     }
 
-    # ,'layout': {
-    #     'annotations': [{
-    #         'x': 0, 'y': 0.85, 'xanchor': 'left', 'yanchor': 'bottom',
-    #         'xref': 'paper', 'yref': 'paper', 'showarrow': False,
-    #         'align': 'left', 'bgcolor': 'rgba(255, 255, 255, 0.5)',
-    #         'text': title
-    #     }]
-    # }
+
 
 
 def get_graph_data(selected_day = None):
@@ -82,16 +82,45 @@ def get_graph_config():
 
 app.layout = html.Div(children=[
 
-    html.H1('Roadway Usage Statistics'),
+    html.Div(className="row", children=[
 
-    dcc.Graph( id='main-graph', figure={'data':get_graph_data(), 'layout':get_graph_layout("")}, config=get_graph_config() ),
-    dcc.Graph( id='monday-graph', figure={'data':get_graph_data(0), 'layout':get_graph_layout("Monday")}, config=get_graph_config() ),
-    dcc.Graph( id='tuesday-graph', figure={'data':get_graph_data(1), 'layout':get_graph_layout("Tuesday")}, config=get_graph_config() ),
-    dcc.Graph( id='wednesday-graph', figure={'data':get_graph_data(2), 'layout':get_graph_layout("Wednesday")}, config=get_graph_config() ),
-    dcc.Graph( id='thursday-graph', figure={'data':get_graph_data(3), 'layout':get_graph_layout("Thursday")}, config=get_graph_config() ),
-    dcc.Graph( id='friday-graph', figure={'data':get_graph_data(4), 'layout':get_graph_layout("Friday")}, config=get_graph_config() ),
-    dcc.Graph( id='saturday-graph', figure={'data':get_graph_data(5), 'layout':get_graph_layout("Saturday")}, config=get_graph_config() ),
-    dcc.Graph( id='sunday-graph', figure={'data':get_graph_data(6), 'layout':get_graph_layout("Sunday")}, config=get_graph_config() ),
+        # Page Header
+        html.Div(className="row header", children=[
+            html.H1('Traffic Dashboard'),
+        ]),
+
+    ]),
+
+    html.Div(className="container", children=[
+
+        html.Div(className="row", children=[
+
+            # Graph
+            html.Div(className="seven columns", children=[
+                dcc.Graph( id='main-graph', 
+                    figure={'data':get_graph_data(), 'layout':get_graph_layout("Ruidoso, NM")}, 
+                    config=get_graph_config() ),
+            ]),
+
+            # Image
+            html.Div(className="five columns", children=[
+                html.Img(src='/assets/image.png', style={'maxWidth': '100%'})
+            ])
+
+        ])
+
+    ]),
+
+    
+    # dcc.Graph( id='monday-graph', figure={'data':get_graph_data(0), 'layout':get_graph_layout("Monday")}, config=get_graph_config() ),
+    # dcc.Graph( id='tuesday-graph', figure={'data':get_graph_data(1), 'layout':get_graph_layout("Tuesday")}, config=get_graph_config() ),
+    # dcc.Graph( id='wednesday-graph', figure={'data':get_graph_data(2), 'layout':get_graph_layout("Wednesday")}, config=get_graph_config() ),
+    # dcc.Graph( id='thursday-graph', figure={'data':get_graph_data(3), 'layout':get_graph_layout("Thursday")}, config=get_graph_config() ),
+    # dcc.Graph( id='friday-graph', figure={'data':get_graph_data(4), 'layout':get_graph_layout("Friday")}, config=get_graph_config() ),
+    # dcc.Graph( id='saturday-graph', figure={'data':get_graph_data(5), 'layout':get_graph_layout("Saturday")}, config=get_graph_config() ),
+    # dcc.Graph( id='sunday-graph', figure={'data':get_graph_data(6), 'layout':get_graph_layout("Sunday")}, config=get_graph_config() ),
+
+    
 
     # generate_table(df)
 ])
@@ -117,6 +146,10 @@ def update_graph(selected_day):
     'layout': get_graph_layout()
 }
 
+
+app.css.append_css({
+    "external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"
+})
 
 
 if __name__ == '__main__':
